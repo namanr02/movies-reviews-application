09790c3fe1fe9320f8ca0eec74300f0d12e1be82
namanr02 <naman.rishi.2002@gmail.com>
2023-07-03 22:15:42 +0530

    initial commit

---
+++ movies-reviews-api/src/main/java/com/moviesreviews/mrs/MRSApplication.java
@@ -0,0 +1,39 @@
+package com.moviesreviews.mrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@RestController
+public class MRSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MRSApplication.class, args);
+	}
+
+	@GetMapping("")
+	public ResponseEntity<Boolean> apiRoot() {
+		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+}
---
+++ movies-reviews-api/src/test/java/com/moviesreviews/mrs/MrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesreviews.mrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
